477f833796c3a395b47cd1a498c370741466b0cd
Guillaume Masse <masgui@gmail.com>
2013-04-18 15:07:02 -0400

    refactor command to tasks
    add HEAD dependency
    config for output
    assert for jquery

---
+++ src/main/scala/SbtBowerPlugin.scala
@@ -0,0 +1,49 @@
+import sbt._
+import Keys._
+import Path._
+import IO._
+
+
+object BowerKeys {
+  val Bower = config("bower") extend (Compile)
+  val frontendDependencies = SettingKey[Seq[FrontendDependency]]("frontend-dependency","frontend dependencies to resolve with bower")
+  val bowerPath = SettingKey[String]("bower-path","where bower is installed")
+}
+
+object SbtBowerPlugin extends Plugin {
+
+  import BowerKeys._
+
+	val install = TaskKey[Unit]("install","install frontend dependencies")
+
+	private def installTask: Project.Initialize[Task[Unit]] = (bowerPath, frontendDependencies, sourceDirectory, streams ) map { (bower, dependencies, source, s ) =>
+	  for { dependency <- dependencies } {
+	    s.log.info("installing %s".format(dependency.install) )
+      createDirectory( source )
+	    Process( bower :: "install" :: dependency.install :: Nil, source ) ! s.log
+	  }
+	}
+
+  lazy val bowerSettings: Seq[Setting[_]] = Seq(
+    libraryDependencies in Bower := Seq.empty,
+    frontendDependencies := Seq.empty,
+    bowerPath := "/usr/local/share/npm/bin/bower",
+    sourceDirectory in Bower <<= (sourceDirectory) (_ / "webapp" )
+  )
+
+  override lazy val projectSettings: Seq[Setting[_]] = inConfig(Bower) (Seq (
+    install <<= installTask
+  ))
+
+  implicit def toFrontendDependency( artifactName: String ) = new FrontendDependency( artifactName )
+}
+
+class FrontendDependency( artifactName: String ) {
+	def `#` ( revision: String ) = new FrontendDependencyWithRevision( artifactName, revision )
+	def HEAD = this
+	def install = artifactName
+}
+
+class FrontendDependencyWithRevision( artifactName: String, revision: String ) extends FrontendDependency( artifactName ) {
+  override def install = "%s#%s".format( super.install, revision )
+}
--- src/sbt-test/sbt-bower/simple-project/project/Build.scala
+++ src/sbt-test/sbt-bower/simple-project/project/Build.scala
@@ -1,19 +1,15 @@
 import sbt._
-import Keys._
 
-import com.github.masseguillaume.SbtBowerPlugin._
-import BowerKeys._
+import SbtBowerPlugin._
 
 object SimpleBuild extends Build {
 	lazy val simple = Project(
 		id = "Simple",
 		base = file("."),
-		settings = Project.defaultSettings ++ bowerSettings ++ Seq(
-			frontendDependencies ++= Seq(
+		settings = Project.defaultSettings ++ SbtBowerPlugin.bowerSettings ++ Seq(
+      BowerKeys.frontendDependencies ++= Seq(
 				"jquery" `#` "1.9.1",
-				"d3" `#` "v3.1.5",
-				"threejs" `#` "r57",
-				"bootstrap" `#` "v2.3.1"
+				"threejs" HEAD
 			)
 		)
 	)
