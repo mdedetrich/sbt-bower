c2d880e0b549d70c09e5f0098df6dcfafc4b5541
Matthew de Detrich <mdedetrich@gmail.com>
2013-10-08 15:24:06 +1100

    - Fixed a few more things

--- project/SbtBowerBuild.scala
+++ project/SbtBowerBuild.scala
@@ -5,10 +5,10 @@ object SbtBowerBuild extends Build with BuildExtra {
 
 	lazy val sbtBower = Project("sbt-bower", file("."), settings = mainSettings)
 
-	lazy val mainSettings: Seq[Project.Setting[_]] = Defaults.defaultSettings ++ /*ScriptedPlugin.scriptedSettings ++ */ Seq(
+	lazy val mainSettings: Seq[Def.Setting[_]] = Defaults.defaultSettings ++ Seq(
 		sbtPlugin := true,
 		name := "sbt-bower",
-		organization := "com.github.mdedetrich",
+		organization := "com.mdedetrich",
 		version := "0.2.0-SNAPSHOT",
 		scalacOptions ++= Seq("-deprecation", "-unchecked")
   )
