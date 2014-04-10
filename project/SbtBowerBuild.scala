4bebaee248592973e5402eddbc67ae3883d749c0
Matthew de Detrich <mdedetrich@gmail.com>
2014-04-10 12:45:13 +1000

    - Updated organization

--- project/SbtBowerBuild.scala
+++ project/SbtBowerBuild.scala
@@ -8,7 +8,7 @@ object SbtBowerBuild extends Build with BuildExtra {
 	lazy val mainSettings: Seq[Def.Setting[_]] = Defaults.defaultSettings ++ Seq(
 		sbtPlugin := true,
 		name := "sbt-bower",
-		organization := "com.github.mdedetrich",
+		organization := "com.mdedetrich",
 		version := "0.2.0",
 		scalacOptions ++= Seq("-deprecation", "-unchecked"),
     libraryDependencies ++= Seq(
